5ee22d8c22b74ee1ba6d68290180ba193ac4fdf8
Heather-Angela1 <124858135+Heather-Angela1@users.noreply.github.com>
2023-03-21 14:40:08 -0700

    Basic JavaScript Projects Assignment

---
+++ basic_javascript_projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+// Declare a variable A and assign a string value containing escaped double quotes
+var A = "She opened her eyes and said \"Hello, Universe\"";
+
+// Display the content of variable A in an alert box
+window.alert(A);
+
+// Declare multiple variables representing a family and their names
+var Family = "The Orlemanns", Mom = "Heather", Dad = "Dave", Daughter = "Jinx", Son = "Kylo";
+
+// Write the value of the variable Daughter (Jinx) to the document
+document.write(Daughter);
+
+// This is a line break
+document.write("<br>");
+
+// Declare a variable named Sent1 with the string value "This is the beginning of the string"
+var Sent1 = "This is the beginning of the string";
+
+// Declare a variable named Sent2 with the string value " and this is the end of the string"
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the Sent1 and Sent2 variables and store the result in the variable named B
+var B = Sent1 + Sent2;
+
+// Use the document.write() method to write the concatenated sentence (variable B) to the HTML document
+document.write(B);
+
+// This is a line break
+document.write("<br>");
+
+// Write the result of an addition expression (3 + 3) to the document
+document.write(12 * 364);
