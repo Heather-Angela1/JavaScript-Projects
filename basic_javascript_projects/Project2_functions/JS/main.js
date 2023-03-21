47ffc0c4c8906f95ad202108f9813cd29cf49ce7
Heather-Angela1 <124858135+Heather-Angela1@users.noreply.github.com>
2023-03-21 15:09:17 -0700

    Project2_Functions Assignment

--- basic_javascript_projects/Project2_functions/JS/main.js
+++ basic_javascript_projects/Project2_functions/JS/main.js
@@ -7,7 +7,11 @@ function updateText() {
     var element1 = document.getElementById("textElement1");
     var element2 = document.getElementById("textElement2");
 
-    // Update the text content of the elements with the assigned values
+    // Concatenate the strings using the += operator
+    text1 += " How are you?";
+    text2 += " Please take a look around.";
+
+    // Update the text content of the elements with the concatenated values
     element1.textContent = text1;
     element2.textContent = text2;
 }
